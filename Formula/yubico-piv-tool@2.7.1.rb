f2f719fb9715c56f07dbeb77b1f62c4f4d2715f5
Karim Benbourenane <karim@benbourenane.com>
2025-08-05 01:02:38 -0400

    Extract version 2.7.1 of yubico-piv-tool

---
+++ Formula/yubico-piv-tool@2.7.1.rb
@@ -0,0 +1,34 @@
+class YubicoPivToolAT271 < Formula
+  desc "Command-line tool for the YubiKey PIV application"
+  homepage "https://developers.yubico.com/yubico-piv-tool/"
+  url "https://developers.yubico.com/yubico-piv-tool/Releases/yubico-piv-tool-2.7.1.tar.gz"
+  sha256 "9813190a5c2560ef7fe8018c03614091e911e0596c5853ef25c82cd9283a444b"
+  license "BSD-2-Clause"
+
+  livecheck do
+    url "https://developers.yubico.com/yubico-piv-tool/Releases/"
+    regex(/href=.*?yubico-piv-tool[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "check" => :build
+  depends_on "cmake" => :build
+  depends_on "gengetopt" => :build
+  depends_on "help2man" => :build
+  depends_on "pkgconf" => :build
+  depends_on "openssl@3"
+
+  uses_from_macos "pcsc-lite"
+  uses_from_macos "zlib"
+
+  def install
+    ENV.append_to_cflags "-I#{Formula["pcsc-lite"].opt_include}/PCSC" unless OS.mac?
+
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_match "yubico-piv-tool #{version}", shell_output("#{bin}/yubico-piv-tool --version")
+  end
+end
